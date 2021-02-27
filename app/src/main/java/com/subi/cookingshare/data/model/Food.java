2f309a478cc0913e7a742ea6eccb67d050b6b403
Phong Subi <phong6786789@gmail.com>
2021-02-28 02:19:48 +0700

    Thêm giao diện Thêm món ăn

---
+++ app/src/main/java/com/subi/cookingshare/data/model/Food.java
@@ -0,0 +1,7 @@
+package com.subi.cookingshare.data.model;
+
+import java.io.Serializable;
+
+public class Food  implements Serializable {
+//    private
+}
--- app/src/main/java/com/subi/cookingshare/screen/fragment/HomeFragment.java
+++ app/src/main/java/com/subi/cookingshare/screen/fragment/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.subi.cookingshare.screen.fragment;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +10,147 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.subi.cookingshare.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private View view = null;
+    private ImageView iv_add_food, ivWay1, ivWay2, ivWay3, ivWay4, ivWay5;
+    private TextView tvWay1, tvWay2, tvWay3, tvWay4, tvWay5;
+    private LinearLayout ln1, ln2, ln3, ln4, ln5;
+    private Dialog dialog;
+    private Spinner spWays;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        init();
+        setOnClickItem();
+        return view;
     }
+
+
+    private void init() {
+        iv_add_food = view.findViewById(R.id.iv_add_food);
+        spWays = view.findViewById(R.id.spWays);
+
+    }
+
+    private void setOnClickItem() {
+        iv_add_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open new Fragment add food
+                dialog = new Dialog(getActivity());
+                dialog.getWindow().getAttributes().windowAnimations = R.style.up_down;
+                dialog.setContentView(R.layout.add_food_fragment);
+                dialog.setCancelable(true);
+                Window window = dialog.getWindow();
+                window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                if (dialog != null && dialog.getWindow() != null) {
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                }
+                initDialog();
+
+
+                //Spinner to show list add Food Ways
+                spinner();
+
+                dialog.show();
+            }
+
+            private void initDialog() {
+                spWays = dialog.findViewById(R.id.spWays);
+                ln1 = dialog.findViewById(R.id.ln_food_ways_1);
+                ln2 = dialog.findViewById(R.id.ln_food_ways_2);
+                ln3 = dialog.findViewById(R.id.ln_food_ways_3);
+                ln4 = dialog.findViewById(R.id.ln_food_ways_4);
+                ln5 = dialog.findViewById(R.id.ln_food_ways_5);
+                ivWay1 = dialog.findViewById(R.id.ivWay1);
+                ivWay2 = dialog.findViewById(R.id.ivWay2);
+                ivWay3 = dialog.findViewById(R.id.ivWay3);
+                ivWay4 = dialog.findViewById(R.id.ivWay4);
+                ivWay5 = dialog.findViewById(R.id.ivWay5);
+                tvWay1 = dialog.findViewById(R.id.tvWay1);
+                tvWay2 = dialog.findViewById(R.id.tvWay2);
+                tvWay3 = dialog.findViewById(R.id.tvWay3);
+                tvWay4 = dialog.findViewById(R.id.tvWay4);
+                tvWay5 = dialog.findViewById(R.id.tvWay5);
+            }
+        });
+    }
+
+    private void spinner() {
+        String listSpinner[] = {"1 bước", "2 bước", "3 bước", "4 bước", "5 bước"};
+        ArrayAdapter<String> spinner = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, listSpinner);
+        //Set drop down item
+        spinner.setDropDownViewResource
+                (android.R.layout.simple_list_item_single_choice);
+        spWays.setAdapter(spinner);
+        //Set event
+        spWays.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView)adapterView.getChildAt(0)).setTextColor(Color.WHITE);
+                switch (i+1) {
+                    case 1:
+                        ln1.setVisibility(View.VISIBLE);
+                        ln2.setVisibility(View.GONE);
+                        ln3.setVisibility(View.GONE);
+                        ln4.setVisibility(View.GONE);
+                        ln5.setVisibility(View.GONE);
+                        break;
+                    case 2:
+                        ln1.setVisibility(View.VISIBLE);
+                        ln2.setVisibility(View.VISIBLE);
+                        ln3.setVisibility(View.GONE);
+                        ln4.setVisibility(View.GONE);
+                        ln5.setVisibility(View.GONE);
+                        break;
+                    case 3:
+                        ln1.setVisibility(View.VISIBLE);
+                        ln2.setVisibility(View.VISIBLE);
+                        ln3.setVisibility(View.VISIBLE);
+                        ln4.setVisibility(View.GONE);
+                        ln5.setVisibility(View.GONE);
+                        break;
+                    case 4:
+                        ln1.setVisibility(View.VISIBLE);
+                        ln2.setVisibility(View.VISIBLE);
+                        ln3.setVisibility(View.VISIBLE);
+                        ln4.setVisibility(View.VISIBLE);
+                        ln5.setVisibility(View.GONE);
+                        break;
+                    case 5:
+                        ln1.setVisibility(View.VISIBLE);
+                        ln2.setVisibility(View.VISIBLE);
+                        ln3.setVisibility(View.VISIBLE);
+                        ln4.setVisibility(View.VISIBLE);
+                        ln5.setVisibility(View.VISIBLE);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
 }
